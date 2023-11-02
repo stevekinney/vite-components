fa376e0ff239799ce4e47f1643b3ac3165c0a73d
Steve Kinney <hello@stevekinney.net>
2023-11-02 14:44:46 -0500

    Set up library mode

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
 import dts from 'vite-plugin-dts';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    libInjectCss(),
     dts({
       include: ['src/components'],
     }),
