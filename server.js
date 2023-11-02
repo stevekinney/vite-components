6c39f22b5758887a9896e187d2e1f474451055b6
Steve Kinney <hello@stevekinney.net>
2023-11-02 07:09:02 -0500

    Implement a proxy

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ let todos = [
 
 // Routes
 app.get('/todos', (req, res) => {
+  console.log('GET /todos');
   res.json(todos);
 });
 
--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/todos': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
+    },
+  },
 });
